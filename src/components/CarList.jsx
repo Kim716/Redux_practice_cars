cc5be4f85c0fc5b90cf73d94bf1f9931251012bc
Kim <yichen.hsieh_c7_alumni@teach4taiwan.org>
2023-05-23 13:33:37 +0800

    feat: 337 - 338 add search /with debounce

--- src/components/CarList.jsx
+++ src/components/CarList.jsx
@@ -1,15 +1,41 @@
 import { useDispatch, useSelector } from "react-redux";
 import { removeCar } from "../store";
+import { useEffect, useState } from "react";
 
 function CarList() {
   const dispatch = useDispatch();
-  const { carsArray } = useSelector((state) => state.cars);
+  // Debounce
+  const { searchTerm, carsArray } = useSelector((state) => state.cars);
+  const [filteredCars, setFilteredCars] = useState([]);
+  // const filteredCars = useSelector(({ cars: { searchTerm, carsArray } }) => {
+  //   return carsArray.filter((car) =>
+  //     car.name.toLowerCase().includes(searchTerm.toLowerCase())
+  //   );
+  // });
 
   const handleDelete = (car) => {
     dispatch(removeCar(car.id));
   };
 
-  const renderedCars = carsArray.map((car) => {
+  // Debounce
+  useEffect(() => {
+    function filterCars() {
+      return carsArray.filter((car) =>
+        car.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    }
+
+    const timeout = setTimeout(() => {
+      console.log(searchTerm);
+      setFilteredCars(filterCars());
+    }, 500);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [searchTerm, carsArray]);
+
+  const renderedCars = filteredCars.map((car) => {
     return (
       <div key={car.id} className="panel">
         <p>
